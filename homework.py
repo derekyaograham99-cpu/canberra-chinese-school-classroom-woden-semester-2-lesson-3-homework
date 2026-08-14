fbef423132d11b1c1ebefd46b0281756a26a7e22
Derek <derek.yao.graham99@gmail.com>
2026-08-14 19:29:51 +1000

    Refactor turtle drawing parameters and colors

--- homework.py
+++ homework.py
@@ -3,7 +3,7 @@
 # Task: look at the code below, and try to explain how it works. Add some more colours in the change_colour function.
 # You can also mess around with the numbers the for loop.
 # Save your changes in this file and commit changes to submit your homework.
-
+# It works by importing a arrow on a little screen which you can give commands to change hwo it moves, its shape and many more
 # To run this script:
 # Since we use a new module called turtle, regular python compiler websites (like online-python.com) will not work.
 # Please test your script in turtle supported online python environments, like
@@ -16,46 +16,30 @@ import random
 
 t = turtle.Turtle()
 screen = turtle.Screen()
-
-t.shape("turtle")
-t.speed(0)  # 0 is fastest, otherwise, 
-# the higher the number is, the faster it is
-t.pensize(1)
+t.shape("square")
+t.color("red")
+t.speed(0)
+t.pensize(15)
 t.pencolor("black")
-screen.bgcolor("lightblue")
+screen.bgcolor("lime")
 
 def change_colour(colour):
     if colour == "red":
         return "green"
     if colour == "green":
-        return "yellow"
-    if colour == "yellow":
-        return "blue"
-    if colour == "blue":
+        return "orange"
+    if colour == "orange":
+        return "purple"
+    if colour == "purple":
         return "red"
 
 current_colour = "red"
 
-for i in range(60):
+for i in range(100):
     current_colour = change_colour(current_colour)
     t.pencolor(current_colour)
-    t.forward(20)
-    t.left(30)
-    t.forward(70)
-    t.left(121)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    t.forward(65)
+    t.left(89)
+    t.forward(75)
+    t.left(80)
+screen.exitonclick()
